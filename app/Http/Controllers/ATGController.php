27c8520e5a46ad5dccf01646923bbf33aae76679
mananbh <mananb@illusiondental.com>
2019-11-29 16:58:05 +0530

    first commit

---
+++ app/Http/Controllers/ATGController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Registration;
+use Session;
+
+class ATGController extends Controller
+{
+    public function index()
+    {
+        return view('welcome');
+    }
+
+  
+    public function store(Request $request)
+    {
+
+        // Validate the request...
+        $this->validate($request,[
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:registration',
+            'pincode' => 'required|regex:/\b\d{6}\b/',
+        ]);
+      
+        $Registration = new Registration;
+        $Registration->name = $request->name;
+        $Registration->email = $request->email;
+        $Registration->pincode = $request->pincode;
+      //  $request->session()->flash('alert-success', 'User was successful added!');
+        $check = $Registration->save();
+        if($check==1){
+            Session::flash('message', 'Added Successfully'); 
+            Session::flash('alert-class','alert-danger'); 
+
+        }else{
+            Session::flash('message', 'Data Not Added'); 
+            Session::flash('alert-danger'); 
+
+        }
+        return redirect()->back();
+
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,77 @@
+
+<link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+<script type="text/javascript" src="{{ asset('js/custom.js') }}"></script>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Register</div>
+
+                <div class="panel-body">
+                    <form class="form-horizontal" method="post" action=" {{URL('/registeruser')}}">
+
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-4 control-label">Name</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('pincode') ? ' has-error' : '' }}">
+                            <label for="pincode" class="col-md-4 control-label">Pincode</label>
+
+                            <div class="col-md-6">
+                                <input id="pincode" type="text" class="form-control" name="pincode" required>
+
+                                @if ($errors->has('pincode'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('pincode') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                @if(session()->has('message'))
+                                    <div class="alert alert-success">
+                                         {{ session()->get('message') }}
+                                     </div>
+                                 @endif
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+       
+    </div>
+</div>
